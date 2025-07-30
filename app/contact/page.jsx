cd96754b0b9c81666dcaeed93fcf5cb8278faf13
nerokome <nerooghenekome@gmail.com>
2025-07-30 14:43:44 +0100

    well

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -35,7 +35,7 @@ const Page = () => {
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.8 }}
           >
-            
+            Get in Touch with SCA
           </motion.h1>
 
           <motion.p
